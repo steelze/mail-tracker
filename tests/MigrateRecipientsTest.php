80d056d1e6102cd44c58f40c301487f73eb580b8
Jon Baker <jbaker@wgm.us>
2021-02-10 09:40:59 -0600

    Testing recipient migration with no names in database

--- tests/MigrateRecipientsTest.php
+++ tests/MigrateRecipientsTest.php
@@ -36,4 +36,32 @@ class MigrateRecipientsTest extends SetUpTest
             'recipient_email' => 'recipient@example.com',
         ]);
     }
+
+    /**
+     * @test
+     */
+    public function it_converts_existing_recipient_values_with_no_name()
+    {
+        Schema::connection((new SentEmail())->getConnectionName())->table('sent_emails', function (Blueprint $table) {
+            $table->string('sender')->nullable();
+            $table->string('recipient')->nullable();
+        });
+        SentEmail::unguard();
+        $tracker = SentEmail::create([
+            'hash' => 'email-hash',
+            'sender' => ' <sender@example.com>',
+            'recipient' => ' <recipient@example.com>',
+        ]);
+        $command = new MigrateRecipients;
+
+        $command->handle();
+
+        $this->assertDatabaseHas('sent_emails', [
+            'id' => $tracker->id,
+            'sender_name' => '',
+            'sender_email' => 'sender@example.com',
+            'recipient_name' => '',
+            'recipient_email' => 'recipient@example.com',
+        ]);
+    }
 }
